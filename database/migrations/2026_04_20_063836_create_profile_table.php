51d79c8287e4bfeb08d76b2fa30ba4802d7c19ee
Isaac-1911 <jetzofsuburbia@gmail.com>
2026-04-20 13:49:59 +0700

    finishing migration tables

---
+++ database/migrations/2026_04_20_063836_create_profile_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->text('vision')->nullable();
+            $table->text('mission')->nullable();
+            $table->string('structure_image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profile');
+    }
+};
